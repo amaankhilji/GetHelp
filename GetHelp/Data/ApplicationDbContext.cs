a8f4ba6d6569305f83290ab3c8d30475604d8ecb
Amaanur Rehman Khilji <akhilji@r1rcm.com>
2023-10-18 09:31:33 +0530

    Add project files.

---
+++ GetHelp/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using GetHelp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GetHelp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Problem> Problem { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            {
+                base.OnModelCreating(modelBuilder);
+
+                modelBuilder.Entity<Problem>().HasData(
+                    new Problem { Id = 1, Name = "rotten oranges", Link = "google.com", Notes = "abc" ,Level=Level.Easy},
+                    new Problem { Id = 2, Name = "2sum", Link = "google.com", Notes = "abc", Level = Level.Medium }
+                   
+                    );
+            }
+        }
+    }
+}
